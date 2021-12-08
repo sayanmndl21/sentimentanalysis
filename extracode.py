e557d5c9d9294ca0333e8974628dd03e6ab8cf9c
sayanmndl21 <sayanmndl21@github.com>
2021-12-08 14:27:37 -0500

    'firstcommit'

---
+++ extracode.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelBinarizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from wordcloud import WordCloud,STOPWORDS
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize,sent_tokenize
+from bs4 import BeautifulSoup
+import spacy
+import re,string,unicodedata
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.stem import LancasterStemmer,WordNetLemmatizer
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from textblob import TextBlob
+from textblob import Word
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+import os
+import warnings
+import string
+import copy
+
+
+class SentimentRNN(nn.Module):
+    def __init__(self,no_layers,vocab_size,hidden_dim,embedding_dim,drop_prob=0.5):
+        super(SentimentRNN,self).__init__()
+ 
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+ 
+        self.no_layers = no_layers
+        self.vocab_size = vocab_size
+    
+        # embedding and LSTM layers
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+        #lstm
+        self.lstm = nn.LSTM(input_size=embedding_dim,hidden_size=self.hidden_dim,
+                           num_layers=no_layers, batch_first=True)
+        
+        
+        # dropout layer
+        self.dropout = nn.Dropout(0.3)
+    
+        # linear and sigmoid layer
+        self.fc = nn.Linear(self.hidden_dim, output_dim)
+        self.sig = nn.Sigmoid()
+        
+    def forward(self,x,hidden):
+        batch_size = x.size(0)
+        # embeddings and lstm_out
+        embeds = self.embedding(x)  # shape: B x S x Feature   since batch = True
+        #print(embeds.shape)  #[50, 500, 1000]
+        lstm_out, hidden = self.lstm(embeds, hidden)
+        
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim) 
+        
+        # dropout and fully connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        
+        # sigmoid function
+        sig_out = self.sig(out)
+        
+        # reshape to be batch_size first
+        sig_out = sig_out.view(batch_size, -1)
+
+        sig_out = sig_out[:, -1] # get last batch of labels
+        
+        # return last sigmoid output and hidden state
+        return sig_out, hidden
+        
+        
+        
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+        # initialized to zero, for hidden state and cell state of LSTM
+        h0 = torch.zeros((self.no_layers,batch_size,self.hidden_dim)).to(device)
+        c0 = torch.zeros((self.no_layers,batch_size,self.hidden_dim)).to(device)
+        hidden = (h0,c0)
+        return hidden
